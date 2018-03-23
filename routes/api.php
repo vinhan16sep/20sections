c1b5680e8555a5a98d6a70eab6e5e4ce95e95c22
vinhan16sep <vinhan16sep@gmail.com>
2018-03-23 17:43:44 +0700

    update constants

--- routes/api.php
+++ routes/api.php
@@ -43,10 +43,10 @@ Route::prefix('v1')->group(function() {
  * In this part, we will build routers for Demo
  * Or whatever ...
  */
-Route::get('/demos', 'DemoController@getDemos');
-Route::middleware('auth.jwt')->post('/demo', 'DemoController@postDemo');
-Route::middleware('auth.jwt')->put('/demo/{id}', 'DemoController@putDemo');
-Route::middleware('auth.jwt')->delete('/demo/{id}', 'DemoController@deleteDemo');
+//Route::get('/demos', 'DemoController@getDemos');
+//Route::middleware('auth.jwt')->post('/demo', 'DemoController@postDemo');
+//Route::middleware('auth.jwt')->put('/demo/{id}', 'DemoController@putDemo');
+//Route::middleware('auth.jwt')->delete('/demo/{id}', 'DemoController@deleteDemo');
 
 Route::post('/user', 'UserController@signup');
 Route::post('/user/signin', 'UserController@signin');
